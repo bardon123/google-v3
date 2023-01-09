fd7441a3af87eea442793695f7e94f4832675696
bardon123 <brandonnmr18@gmail.com>
2023-01-09 14:38:30 +0800

    add authentication and get user data from google

--- components/User.js
+++ components/User.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { UseSession, signIn, signOut, useSession } from "next-auth/react";
 
 export default function User() {
-  return <div>User</div>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <button
+          onClick={signOut}
+          className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        >
+          Sign Out
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        onClick={signIn}
+      >
+        {" "}
+        Sign in
+      </button>
+    </>
+  );
 }
